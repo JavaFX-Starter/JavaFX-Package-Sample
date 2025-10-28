92abbe4dafb3c175216c148a2d77d3509fa7f97e
icuxika <icuxika@outlook.com>
2025-10-28 14:12:26 +0800

    修复模块化构建失败的问题

--- BuildModular.ps1
+++ BuildModular.ps1
@@ -4,11 +4,11 @@ if (Test-Path -Path .\target\) {
 
 mvn -Pmodular package
 
-jdeps.exe --list-deps --ignore-missing-deps --multi-release 9 --module-path .\target\jars\ -m sample
-(jdeps.exe --list-deps --ignore-missing-deps --multi-release 9 --module-path .\target\jars\ -m sample | ForEach-Object {$_.Trim()} | Where-Object {$_}) -join ","
+jdeps.exe --list-deps --ignore-missing-deps --multi-release 9 --module-path ".\target\jars\;.\target\modules\" -m sample
+(jdeps.exe --list-deps --ignore-missing-deps --multi-release 9 --module-path ".\target\jars\;.\target\modules\" -m sample | ForEach-Object {$_.Trim()} | Where-Object {$_}) -join ","
 
-jdeps.exe --list-reduced-deps --ignore-missing-deps --multi-release 9 --module-path .\target\jars\ -m sample
-(jdeps.exe --list-reduced-deps --ignore-missing-deps --multi-release 9 --module-path .\target\jars\ -m sample | ForEach-Object {$_.Trim()} | Where-Object {$_}) -join ","
+jdeps.exe --list-reduced-deps --ignore-missing-deps --multi-release 9 --module-path ".\target\jars\;.\target\modules\" -m sample
+(jdeps.exe --list-reduced-deps --ignore-missing-deps --multi-release 9 --module-path ".\target\jars\;.\target\modules\" -m sample | ForEach-Object {$_.Trim()} | Where-Object {$_}) -join ","
 
 
 mvn -Pmodular exec:exec@image
